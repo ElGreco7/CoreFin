e06ab986329f1150f98edbdbb53f95d8d15ecca2
Francisco <franciscoemanuelfe2003@gmail.com>
2026-05-17 20:06:08 -0300

    HotFix Superuser

--- back/build.sh
+++ back/build.sh
@@ -11,12 +11,11 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model
 import os
 User = get_user_model()
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@corefin.com')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
 password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
-if password and not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
+if email and password and not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(email=email, password=password)
     print('Superusuário criado.')
 else:
-    print('Superusuário já existe ou senha não definida.')
+    print('Superusuário já existe ou variáveis não definidas.')
 "
